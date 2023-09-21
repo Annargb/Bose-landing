c1f6477d6191b1e3b9267d374333c2b2df72ce4b
Annargb <annasamoylenko2015@gmail.com>
2023-09-21 03:24:44 +0200

    fix mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
+  // const menuLink = document.querySelectorAll(".menu-link");
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -9,14 +10,21 @@
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
 
-    const scrollLockMethod = !isMenuOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
+    // const scrollLockMethod = !isMenuOpen
+    //   ? "disableBodyScroll"
+    //   : "enableBodyScroll";
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", onModalClose);
+
+  function onModalClose(event) {
+    if (event.target.classList.contains("menu-link")) {
+      mobileMenu.classList.remove("is-open");
+    }
+  }
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
